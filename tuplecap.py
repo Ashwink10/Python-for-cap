11fb5c5d6432d1a99adbc162fc99045e8fef23c1
Ashwink10 <83913080+Ashwink10@users.noreply.github.com>
2021-07-11 18:44:17 +0530

    Add files via upload

---
+++ tuplecap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 11 17:33:25 2021
+
+@author: Ashwin
+"""
+
+tuple1=(1,2,'ashwin','vishal',2)
+tuple2=(5,6,7)
+print(tuple1)
+x=tuple1.count(2)
+print(x)
+y=tuple1.index('ashwin')
+print(y)
